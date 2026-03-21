b784114de9ccb17bfa76435d5471925011533c02
DyloreAquino <lutherjerold@gmail.com>
2026-03-21 09:08:44 +0800

    Changes to spawn of pickup item

--- kailog-demo/Stations/cleaning_station.gd
+++ kailog-demo/Stations/cleaning_station.gd
@@ -29,5 +29,5 @@ func receive_item(item: Item) -> void:
 func _spawn_pickup(item: Item) -> void:
 	var pickup := preload("res://Items/pickup_item.tscn").instantiate()
 	pickup.item = item
-	pickup.global_position = global_position + Vector2(0, 40)
+	pickup.global_position = global_position + Vector2(randi_range(-100, 100), randi_range(-100, 100))
 	get_parent().add_child(pickup)
