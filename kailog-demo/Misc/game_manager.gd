7474366a62bb561e8ddc3b2ea7a8cb46ca192f1e
thisotherguyalso <161091156+thisotherguyalso@users.noreply.github.com>
2026-04-25 09:06:31 +0800

    actually be able to change level

--- kailog-demo/Misc/game_manager.gd
+++ kailog-demo/Misc/game_manager.gd
@@ -15,8 +15,9 @@ signal round_ended(results: RoundResults)
 	"res://Levels/level_2.tscn",
 	"res://Levels/level_3.tscn"
 ]
+@export var next_level: String = ""
 
-var current_round: int = 0
+var current_round: int = 1
 var current_flood_level: float = 50.0
 
 
@@ -85,6 +86,6 @@ func _show_evaluation(results: RoundResults) -> void:
 func proceed_to_next_round() -> void:
 	for item in player.inventory.get_items():
 		player.inventory.remove_item(item)	
-	if current_round < levels.size():
-		get_tree().change_scene_to_file(levels[current_round])
+	if next_level != "":
+		get_tree().change_scene_to_file(next_level)
 		start_round()
